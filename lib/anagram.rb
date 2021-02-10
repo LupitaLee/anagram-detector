691ed9c21c5030f3b0176a5f87289c6202a2f477
Lupita <lupitarivera8899@gmail.com>
2021-02-10 12:28:21 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+    attr_accessor :word     #Your class, Anagram
+    
+    def initialize(word)        #should take a word on initialization,
+      @word = word
+    end
+
+    def match(array)
+    #    binding.pry
+       array.select do |array_word|
+        @word.split("").sort == array_word.split("").sort
+       end
+    end
+end
+
+
+
+#       # should detect no matches
+#   should detect a simple anagram
+#   should detect an anagram
+#   should detect multiple anagrams
